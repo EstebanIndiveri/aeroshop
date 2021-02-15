135659a8396e6c35359a8a40a55ff33717edcb1c
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-14 23:41:48 -0300

    fix effect

--- src/views/OrderScreen.js
+++ src/views/OrderScreen.js
@@ -41,7 +41,7 @@ function OrderScreen({match}) {
             }
             document.body.appendChild(script);
         }
-        if(!order || successPay){
+        if(!order || successPay || order?._id !==orderId){
             dispatch({type:ORDER_PAY_RESET})
             dispatch(getOrderDetails(orderId))
         } else if(!order.isPaid){
