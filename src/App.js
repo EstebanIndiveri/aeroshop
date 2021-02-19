82e0a297196a0a35de9ba5ad4c13d7d33e83f8f8
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-18 21:38:18 -0300

    add productlistscreen

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import PlaceOrderScreen from './views/PlaceOrderScreen';
 import OrderScreen from './views/OrderScreen';
 import UserListScreen from './views/UserListScreen';
 import UserEditScreen from './views/UserEditScreen';
+import ProductListScreen from './views/ProductList';
 
 const App = ()=> {
   return (
@@ -34,6 +35,7 @@ const App = ()=> {
             <Route path='/product/:id' component={ProductScreen}/>
             <Route path="/cart/:id?" component={CartScreen}/>
             <Route path="/admin/userlist" component={UserListScreen}/>
+            <Route path="/admin/productlist" component={ProductListScreen}/>
             <Route path="/admin/user/:id/edit" component={UserEditScreen}/>
             <Route path='/' component={HomeScreen} exact/>
           </Container>
