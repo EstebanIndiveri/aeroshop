16cdf4bf44b5e4c51c9211de83c5243ec89234ec
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-21 13:34:59 -0300

    upload product

--- src/views/ProductEditScreen.js
+++ src/views/ProductEditScreen.js
@@ -4,8 +4,9 @@ import {Form,Button} from 'react-bootstrap'
 import {useDispatch,useSelector} from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import {listProductDetails} from '../actions/productActions'
+import {listProductDetails,updateProduct} from '../actions/productActions'
 import FormContainer from '../components/FormContainer';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 const ProductEditScreen = ({match,history}) => {
     const productId=match.params.id;
@@ -24,25 +25,45 @@ const ProductEditScreen = ({match,history}) => {
     const productDetail=useSelector(state=>state.productDetail)
     const {loading,error,product}=productDetail;
 
+
+    const productUpdate=useSelector(state=>state.productUpdate)
+    const {loading:loadingUpdate,error:errorUpdate,success:successUpdate}=productUpdate;
+
     useEffect(()=>{
-        
-        if(!product.name || product._id!==productId){
-            dispatch(listProductDetails(productId))
+        if(successUpdate){
+            dispatch({type:PRODUCT_UPDATE_RESET})
+            history.push('/admin/productlist')
         }else{
-            setName(product.name)
-            setPrice(product.email)
-            setBrand(product.brand);
-            setImage(product.image);
-            setCategory(product.category);
-            setCountInStock(product.countInStock)
-            setDescription(product.description)
+
+        
+            if(!product.name || product._id!==productId){
+                dispatch(listProductDetails(productId))
+            }else{
+                setName(product.name)
+                setPrice(product.email)
+                setBrand(product.brand);
+                setImage(product.image);
+                setCategory(product.category);
+                setCountInStock(product.countInStock)
+                setDescription(product.description)
+            }
         }
-    
-    },[product,dispatch,productId,history])
+    },[product,dispatch,productId,history,successUpdate])
 
     const submitHandler=(e)=>{
         e.preventDefault();
+
         // Update product
+        dispatch(updateProduct({
+            _id:productId,
+            name,
+            price,
+            image,
+            brand,
+            category,
+            description,
+            countInStock
+        }))
     }
     return (
         <Fragment>
@@ -51,6 +72,8 @@ const ProductEditScreen = ({match,history}) => {
             </Link>
         <FormContainer>
             <h1>Edit Product</h1>
+            {loadingUpdate&&<Loader/>}
+            {errorUpdate&&<Message variant="danger">{errorUpdate}</Message>}
             {loading?<Loader/>:error?<Message variant="danger">{error}</Message>:(
              <Form onSubmit={submitHandler}>
              <Form.Group controlId="name">
